775ca3214830cb5e63be40e019c0088d3f1035e8
Charles Jarvis <csjarvis43@gmail.com>
2022-09-26 12:13:33 -0600

    solution

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// JS algorithm practice, done in branches
+function minSubArrayLen(arr, sum){
+
+    let total = 0
+    let start = 0
+    let end = 0
+    let minLen = Infinity
+
+    while (start < arr.length) {
+        if (total < sum && end < arr.length) {
+            total += arr[end]
+            end++
+        }
+
+        else if (total >= sum) {
+            minLen = Math.min(minLen, end - start)
+            total -= arr[start]
+            start++
+        }
+
+        else{ break }
+    }
+
+    return minLen === Infinity ? 0 : minLen
+
+}
+
+console.log(minSubArrayLen([1, 2, 3, 3, 2,4,5,6,6,7,4,2], 17))

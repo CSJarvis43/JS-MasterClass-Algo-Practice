a76b57c0dd784a03dcbcd67832f5e57b7082833f
Charles Jarvis <csjarvis43@gmail.com>
2022-09-27 16:09:33 -0600

    solution for recursive palindrome checker

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// JS algorithm practice, done in branches
+function isPalindrome(str) {
+
+
+    function reverse(str) {
+        if (str.length <= 1) return str
+
+        return reverse(str.slice(1)) + str[0]
+    }
+
+    if (str === reverse(str)) {
+        return true
+    } else return false
+}
+
+console.log(isPalindrome('tacocat'))
